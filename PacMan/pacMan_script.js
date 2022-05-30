27ac3ee62218ae956d16732bae6118cbd0160a6b
SunKyu Choi <zjvl324@gmail.com>
2022-05-30 15:00:31 +0900

    자잘한 업뎃

--- PacMan/pacMan_script.js
+++ PacMan/pacMan_script.js
@@ -14,3 +14,20 @@ on_off.onclick = function () {
 		audio.pause();
 	}
 }
+
+var keys = {};
+window.addEventListener("keydown",
+    function(e){
+        keys[e.keyCode] = true;
+        switch(e.keyCode){
+            case 37: case 39: case 38:  case 40: // 키 값
+            case 32: e.preventDefault(); break; // Space bar
+            default: break;
+        }
+    },
+false);
+window.addEventListener('keyup',
+    function(e){
+        keys[e.keyCode] = false;
+    },
+false);
